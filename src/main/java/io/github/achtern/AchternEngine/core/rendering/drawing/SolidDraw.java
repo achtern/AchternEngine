5ff3f3d21ab90f6e07c3974732ff306640aa3bc5
Christian Gärtner <chrisdagardner@gmail.com>
2014-06-18 19:00:59 +0200

    Minor clean up

--- src/main/java/io/github/achtern/AchternEngine/core/rendering/drawing/SolidDraw.java
+++ src/main/java/io/github/achtern/AchternEngine/core/rendering/drawing/SolidDraw.java
@@ -1,17 +1,11 @@
 package io.github.achtern.AchternEngine.core.rendering.drawing;
 
 import io.github.achtern.AchternEngine.core.rendering.Vertex;
-import io.github.achtern.AchternEngine.core.rendering.drawing.DrawStrategy;
 import io.github.achtern.AchternEngine.core.rendering.mesh.MeshData;
-import org.lwjgl.opengl.GL20;
 
 import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.opengl.GL15.GL_ARRAY_BUFFER;
-import static org.lwjgl.opengl.GL15.GL_ELEMENT_ARRAY_BUFFER;
-import static org.lwjgl.opengl.GL15.glBindBuffer;
-import static org.lwjgl.opengl.GL20.glDisableVertexAttribArray;
-import static org.lwjgl.opengl.GL20.glEnableVertexAttribArray;
-import static org.lwjgl.opengl.GL20.glVertexAttribPointer;
+import static org.lwjgl.opengl.GL15.*;
+import static org.lwjgl.opengl.GL20.*;
 import static org.lwjgl.opengl.GL30.glBindVertexArray;
 
 public class SolidDraw implements DrawStrategy {
@@ -30,7 +24,7 @@ public class SolidDraw implements DrawStrategy {
         glBindBuffer(GL_ARRAY_BUFFER, data.getVbo());
 
         // Position
-        GL20.glVertexAttribPointer(0, 3, GL_FLOAT, false, Vertex.SIZE * 4, 0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, false, Vertex.SIZE * 4, 0);
         // Texture Coordinates
         glVertexAttribPointer(1, 2, GL_FLOAT, false, Vertex.SIZE * 4, 12);
         // Normals
