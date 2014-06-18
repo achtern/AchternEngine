ce97d554ae87a369d88351bbd250a13d51a3ebbb
Christian Gärtner <christiangaertner.film@googlemail.com>
2014-06-18 20:26:07 +0200

    AxisDebugger is now working... Need to color the arrows though

--- src/main/java/io/github/achtern/AchternEngine/core/entity/debug/AxisDebugger.java
+++ src/main/java/io/github/achtern/AchternEngine/core/entity/debug/AxisDebugger.java
@@ -1,8 +1,10 @@
 package io.github.achtern.AchternEngine.core.entity.debug;
 
+import io.github.achtern.AchternEngine.core.Node;
 import io.github.achtern.AchternEngine.core.RenderEngine;
 import io.github.achtern.AchternEngine.core.Transform;
 import io.github.achtern.AchternEngine.core.entity.Figure;
+import io.github.achtern.AchternEngine.core.math.Quaternion;
 import io.github.achtern.AchternEngine.core.rendering.Color;
 import io.github.achtern.AchternEngine.core.rendering.Material;
 import io.github.achtern.AchternEngine.core.rendering.mesh.Mesh;
@@ -11,47 +13,32 @@ import io.github.achtern.AchternEngine.core.resource.ResourceLoader;
 
 import java.io.IOException;
 
-public class AxisDebugger extends Figure {
+public class AxisDebugger extends Node {
 
-    public Figure[] axes = new Figure[3];
-
-    public AxisDebugger() {
-        super("AxisDebugger");
-    }
-
+    /**
+     * Create a new AxisDebugger.
+     *
+     * @param name The name of the node/axisdebugger
+     */
     public AxisDebugger(String name) {
         super(name);
-        init();
     }
 
-    private void init() {
-        Arrow arrow;
+    /**
+     * Creates an AxisDebugger.
+     */
+    public AxisDebugger() {
+        this("AxisDebugger");
 
+        pushFigure(new Arrow(), Color.RED, 0);
+        pushFigure( new Arrow(), Color.GREEN, 1);
+        pushFigure(new Arrow(), Color.BLUE, 2);
 
-        arrow = new Arrow();
-        pushFigure(arrow, Color.RED, 0);
-
-        arrow = new Arrow();
-        pushFigure(arrow, Color.GREEN, 1);
-
-
-        arrow = new Arrow();
-        pushFigure(arrow, Color.BLUE, 2);
     }
 
     @Override
     public void render(Shader shader, RenderEngine renderEngine) {
-        System.out.println("Called");
-        for (int i = 0; i < axes.length; i++) {
-            Figure f = axes[i];
-            f.setParent(getParent());
-
-            if (i == 0) f.getTransform().rotate(Transform.X_AXIS, 90);
-            else if (i == 1) f.getTransform().rotate(Transform.Y_AXIS, 90);
-            else if (i == 2) f.getTransform().rotate(Transform.Z_AXIS, 90);
-
-            f.render(shader, renderEngine);
-        }
+        super.render(shader, renderEngine);
     }
 
     private void pushFigure(Mesh mesh, Color color, int i) {
@@ -69,6 +56,21 @@ public class AxisDebugger extends Figure {
 
         f.setMaterial(m);
 
-        axes[i] = f;
+        String name = "";
+
+        if (i == 0) name = "X-Axis";
+        else if (i == 1) name = "Y-Axis";
+        else if (i == 2) name = "Z-Axis";
+
+        Node n = new Node("AxisDebugger/" + name);
+
+        n.add(f);
+
+        add(n);
+
+        if (i == 0)      n.getTransform().setRotation(new Quaternion(Transform.X_AXIS, (float) Math.toRadians(90)));
+        else if (i == 1) n.getTransform().setRotation(new Quaternion(Transform.Y_AXIS, (float) Math.toRadians(90)));
+        else if (i == 2) n.getTransform().setRotation(new Quaternion(Transform.Z_AXIS, (float) Math.toRadians(90)));
+
     }
 }
