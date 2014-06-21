404a0e4d1accf2b32324256173d1643b5ce6fece
Christian Gärtner <chrisdagardner@gmail.com>
2014-06-21 14:44:22 +0200

    Create ByteBuffers for Textures from Color

--- src/main/java/io/github/achtern/AchternEngine/core/rendering/generator/ImageGenerator.java
+++ src/main/java/io/github/achtern/AchternEngine/core/rendering/generator/ImageGenerator.java
@@ -2,9 +2,11 @@ package io.github.achtern.AchternEngine.core.rendering.generator;
 
 import io.github.achtern.AchternEngine.core.math.Vector2f;
 import io.github.achtern.AchternEngine.core.rendering.Color;
+import io.github.achtern.AchternEngine.core.util.UBuffer;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.nio.ByteBuffer;
 
 public class ImageGenerator {
 
@@ -25,6 +27,32 @@ public class ImageGenerator {
         g.dispose();
 
         return image;
+    }
+
+    public static ByteBuffer bytesFromColor(Color color) {
+        return bytesFromColor(new Vector2f(1, 1), color);
+    }
+
+    public static ByteBuffer bytesFromColor(Vector2f dimensions, Color color) {
+        ByteBuffer buffer = UBuffer.createByteBuffer((int) (dimensions.getX() * dimensions.getY() * 4));
+
+        for(int y = 0; y < dimensions.getY(); y++) {
+            for(int x = 0; x < dimensions.getX(); x++) {
+
+                int pixel = color.toInt();
+
+                buffer.put((byte)((pixel) & 0xFF)); // R
+                buffer.put((byte)((pixel >> 8) & 0xFF)); // G
+                buffer.put((byte)((pixel >> 16) & 0xFF)); // B
+                buffer.put((byte)((pixel >> 24) & 0xFF)); // Alpha
+
+            }
+        }
+
+        buffer.flip();
+
+
+        return buffer;
     }
 
 }
