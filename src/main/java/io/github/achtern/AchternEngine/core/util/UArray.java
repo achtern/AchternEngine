c3812ab68dd2f0a47a2bd79cbc6672b3d98f1444
Christian Gärtner <christiangaertner.film@googlemail.com>
2014-06-21 15:18:18 +0200

    Added javadoc to util classes and simplified them

--- src/main/java/io/github/achtern/AchternEngine/core/util/UArray.java
+++ src/main/java/io/github/achtern/AchternEngine/core/util/UArray.java
@@ -1,7 +1,15 @@
 package io.github.achtern.AchternEngine.core.util;
 
+/**
+ * Utility for Arrays
+ */
 public class UArray {
 
+    /**
+     * Joins an array of strings into one.
+     * @param strings The strings to get joined
+     * @return the joined String
+     */
     public static String join(String[] strings) {
         StringBuilder joined = new StringBuilder();
 
--- src/main/java/io/github/achtern/AchternEngine/core/util/UBuffer.java
+++ src/main/java/io/github/achtern/AchternEngine/core/util/UBuffer.java
@@ -8,9 +8,18 @@ import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
 
+/**
+ * Utility for Buffers
+ */
 public class UBuffer {
 
 
+    /**
+     * Creates a float buffer from {@link io.github.achtern.AchternEngine.core.rendering.Vertex} array;
+     * compatible with LWJGL OpenGL binding.
+     * @param vertices The vertices to buffer
+     * @return the buffer
+     */
     public static FloatBuffer create(Vertex[] vertices) {
 
         FloatBuffer buffer = createFloatBuffer(vertices.length * Vertex.SIZE);
@@ -32,6 +41,11 @@ public class UBuffer {
 
     }
 
+    /**
+     * Create IntBuffer from integers (or int array)
+     * @param integers the integers to buffer
+     * @return the buffer
+     */
     public static IntBuffer create(int... integers) {
         IntBuffer buffer = createIntBuffer(integers.length);
         buffer.put(integers);
@@ -39,6 +53,12 @@ public class UBuffer {
         return buffer;
     }
 
+    /**
+     * Creates a float buffer from {@link io.github.achtern.AchternEngine.core.math.Matrix4f};
+     * compatible with LWJGL OpenGL binding.
+     * @param matrix The matrix to buffer
+     * @return the buffer
+     */
     public static FloatBuffer create(Matrix4f matrix) {
         FloatBuffer buffer = createFloatBuffer(16); // 4 * 4
 
@@ -51,14 +71,31 @@ public class UBuffer {
         return buffer;
     }
 
+    /**
+     * Create an integer buffer.
+     * The size will get << shifted by 2 (integers allocate 2 bytes!)
+     * @param size the size of the buffer (amount of integers!)
+     * @return the buffer
+     */
     public static IntBuffer createIntBuffer(int size) {
         return createByteBuffer(size << 2).asIntBuffer();
     }
 
+    /**
+     * Create an float buffer.
+     * The size will get << shifted by 2 (floats allocate 2 bytes!)
+     * @param size the size of the buffer (amount of integers!)
+     * @return the buffer
+     */
     public static FloatBuffer createFloatBuffer(int size) {
         return createByteBuffer(size << 2).asFloatBuffer();
     }
 
+    /**
+     * Create a simple ByteBuffer.
+     * @param size The size of the buffer
+     * @return the buffer
+     */
     public static ByteBuffer createByteBuffer(int size) {
         return ByteBuffer.allocateDirect(size).order(ByteOrder.nativeOrder());
     }
