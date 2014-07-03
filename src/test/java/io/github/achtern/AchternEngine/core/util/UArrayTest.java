cffdb1b71fa61b056a536f75378734bae55a78b0
Christian Gärtner <chrisdagardner@gmail.com>
2014-07-03 17:50:40 +0200

    Added concate array util method + test

--- src/test/java/io/github/achtern/AchternEngine/core/util/UArrayTest.java
+++ src/test/java/io/github/achtern/AchternEngine/core/util/UArrayTest.java
@@ -2,6 +2,7 @@ package io.github.achtern.AchternEngine.core.util;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class UArrayTest {
@@ -26,5 +27,23 @@ public class UArrayTest {
 
         assertEquals("123 4", UArray.join(array1));
         assertEquals("ABC D", UArray.join(array2));
+    }
+
+    public void testConcat() {
+
+        Integer[] arr1 = new Integer[] {
+                1, 2, 3
+        };
+
+        Integer[] arr2 = new Integer[] {
+                4, 5, 6
+        };
+
+        Integer[] expected = new Integer[] {
+                1, 2, 3, 4, 5, 6
+        };
+
+        assertArrayEquals(expected, UArray.concat(arr1, arr2));
+
     }
 }
