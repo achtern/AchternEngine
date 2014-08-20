337e2ec9709db1beba56f9406cfd8075d847abd5
Christian Gärtner <chrisdagardner@gmail.com>
2014-08-20 18:27:15 +0200

    Updated javadoc in ResourceLoader to reflect the recent changes

--- src/main/java/io/github/achtern/AchternEngine/core/resource/ResourceLoader.java
+++ src/main/java/io/github/achtern/AchternEngine/core/resource/ResourceLoader.java
@@ -22,10 +22,37 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The {@link io.github.achtern.AchternEngine.core.resource.ResourceLoader} is the main entry point
+ * to the FileSystem and other Resources.
+ * The ResourceLoader allows you to load {@link io.github.achtern.AchternEngine.core.rendering.mesh.Mesh}es and
+ * {@link io.github.achtern.AchternEngine.core.scenegraph.entity.Figure}s,
+ * {@link io.github.achtern.AchternEngine.core.rendering.shader.Shader}s
+ * ({@link io.github.achtern.AchternEngine.core.resource.fileparser.GLSLProgram}),
+ * {@link io.github.achtern.AchternEngine.core.rendering.texture.Texture}s and
+ * any type of String from a file.
+ * You can easily load your own resources from any type of file.
+ * You can get {@link java.net.URL}s and {@link java.io.InputStream}s for any filename or write your
+ * own {@link io.github.achtern.AchternEngine.core.resource.loader.Loader} to load text-based files.
+ * The ResourceLoader looks into given {@link io.github.achtern.AchternEngine.core.resource.ResourceLocation}s in
+ * order to get URLs and InputStreams for the given name.
+ * By default the Classpath, Bundled Textures, Meshes, Figures, Shaders and the Local File System
+ * are added as ResourceLocation by default. You can add your own by using {@link #addResourceLocation(ResourceLocation)}
+ * or remove all {@link #clearResourceLocations()}.
+ *
+ * The ResourceLoader uses a very basic {@link io.github.achtern.AchternEngine.core.resource.ResourceCache}s in order
+ * to avoid re-reading data from disk. These caches - however - are not caching object, only the data.
+ */
 public class ResourceLoader {
 
+    /**
+     * The Main Logger of the ResourceLoader
+     */
     public static final Logger LOGGER = LoggerFactory.getLogger(ResourceLoader.class);
 
+    /**
+     * Extension used by Shader Programs
+     */
     public static final String SHADER_PROGRAM_EXT = ".yaml";
 
     private static ResourceCache<TexturableData> textureCache = new ResourceCache<TexturableData>();
@@ -99,7 +126,7 @@ public class ResourceLoader {
 
     /**
      * Pre Load a Mesh, good at startup, to allow getting the Mesh at runtime.
-     * Binds the mesh to the Graphics API.
+     * @see #getMesh(String)
      * @param name The relative path (to various ResourceLocations) of the filename
      * @throws IOException
      */
@@ -109,7 +136,7 @@ public class ResourceLoader {
 
     /**
      * Pre Load a Texture, good at startup, to allow getting the Texture at runtime.
-     * Binds the texture to the Graphics API.
+     * @see #getTexture(String)
      * @param name The relative path (to various ResourceLocations) of the filename
      * @throws IOException
      */
@@ -120,6 +147,7 @@ public class ResourceLoader {
     /**
      * Pre Load a Shader source file.
      * Only parses the sourcefile, no binding or uniform adding at this stage!
+     * @see #getShader(String)
      * @param name The relative path (to various ResourceLocations) of the filename
      * @throws IOException
      */
@@ -128,22 +156,29 @@ public class ResourceLoader {
     }
 
     /**
-     * Loads a obj file, parses it, optimises it and converts it into renderable Mesh
-     * (uses a internal cache if the texture has been loaded previously)
+     * Loads a .obj file, parses it, optimises it and converts it into renderable {@link io.github.achtern.AchternEngine.core.rendering.mesh.Mesh}
+     * When loading from cache only the data will get reused, not the object itself and it
+     * will have to use new buffers (handled automatically).
+     * An IOException is thrown on read errors and other Exceptions might be
+     * thrown from the GLSLProgramLoader itself.
      * @param name The relative path (to various ResourceLocations) of the filename
-     * @return A readable Stream | null if not exists
-     * @throws IOException if resource not found
+     * @return Mesh (new instance)
+     * @throws Exception
      */
     public static Mesh getMesh(String name) throws Exception {
         return getMesh(name, false);
     }
 
     /**
-     * Loads a obj file, parses it, optimises it and converts it into renderable Mesh
+     * Loads a .obj file, parses it, optimises it and converts it into renderable {@link io.github.achtern.AchternEngine.core.rendering.mesh.Mesh}
+     * When loading from cache only the data will get reused, not the object itself and it
+     * will have to use new buffers (handled automatically).
+     * An IOException is thrown on read errors and other Exceptions might be
+     * thrown from the GLSLProgramLoader itself.
      * @param name The relative path (to various ResourceLocations) of the filename
      * @param forceLoading if set to true the file will get read again and not read from cache
-     * @return A readable Stream | null if not exists
-     * @throws IOException if resource not found
+     * @return Mesh (new instance)
+     * @throws Exception
      */
     public static Mesh getMesh(String name, boolean forceLoading) throws Exception {
         // With Mesh we have to be carefull. The LineBasedParser parse,
@@ -164,11 +199,12 @@ public class ResourceLoader {
     }
 
     /**
-     * Loads a image file and converts it into a Texture
-     * (uses a internal cache if the texture has been loaded previously)
-     * The dimensions/size will be the size of the source image.
+     * Loads a image file and converts it into a {@link io.github.achtern.AchternEngine.core.rendering.texture.Texture}.
+     * If the same image has been loaded previously the
+     * {@link io.github.achtern.AchternEngine.core.contracts.TexturableData} will be
+     * used to create the new {@link io.github.achtern.AchternEngine.core.rendering.texture.Texture} object.
      * @param name The relative path (to various ResourceLocations) of the filename
-     * @return A readable Stream | null if not exists
+     * @return A new Texture Object
      * @throws IOException if resource not found
      */
     public static Texture getTexture(String name) throws IOException {
@@ -176,11 +212,15 @@ public class ResourceLoader {
     }
 
     /**
-     * Loads a image file and converts it into a Texture
-     * (uses a internal cache if the texture has been loaded previously)
+     * Loads a image file and converts it into a {@link io.github.achtern.AchternEngine.core.rendering.texture.Texture}.
+     * If the same image has been loaded previously the
+     * {@link io.github.achtern.AchternEngine.core.contracts.TexturableData} will be
+     * used to create the new {@link io.github.achtern.AchternEngine.core.rendering.texture.Texture} object.
+     * The {@link io.github.achtern.AchternEngine.core.rendering.Dimension} can be used to modify the width and height
+     * of the Texture. The image will get resized.
      * @param name The relative path (to various ResourceLocations) of the filename
-     * @param dimension The dimension of the new texture (if null the one of the images will get taken)
-     * @return A readable Stream | null if not exists
+     * @param dimension The dimension of the new texture
+     * @return A new Texture Object
      * @throws IOException if resource not found
      */
     public static Texture getTexture(String name, Dimension dimension) throws IOException {
@@ -188,11 +228,16 @@ public class ResourceLoader {
     }
 
     /**
-     * Loads a image file and converts it into a Texture
+     * Loads a image file and converts it into a {@link io.github.achtern.AchternEngine.core.rendering.texture.Texture}.
+     * If the same image has been loaded previously the
+     * {@link io.github.achtern.AchternEngine.core.contracts.TexturableData} will be
+     * used to create the new {@link io.github.achtern.AchternEngine.core.rendering.texture.Texture} object.
+     * The {@link io.github.achtern.AchternEngine.core.rendering.Dimension} can be used to modify the width and height
+     * of the Texture. The image will get resized.
      * @param name The relative path (to various ResourceLocations) of the filename
      * @param dimension The dimension of the new texture
      * @param forceLoading if set to true the file will get read again and not read from cache
-     * @return A readable Stream | null if not exists
+     * @return A new Texture Object
      * @throws IOException if resource not found
      */
     public static Texture getTexture(String name, Dimension dimension, boolean forceLoading) throws IOException {
@@ -218,8 +263,9 @@ public class ResourceLoader {
     }
 
     /**
-     * Loads a shader File and prepares it for OpenGL
+     * Loads a shader File and prepares it for OpenGL.
      * (uses a internal cache if the shader has been loaded previously)
+     * @see #getShader(String, boolean)
      * @param name The relative path (to various ResourceLocations) of the filename
      * @return A readable Stream | null if not exists
      * @throws IOException if resource not found
@@ -229,12 +275,30 @@ public class ResourceLoader {
     }
 
     /**
-     * Loads a shader File and prepares it for OpenGL
+     * Loads a shader File and prepares it for OpenGL.
+     * (uses a internal cache if the shader has been loaded previously)
+     * This uses the default LineBasedParser ({@link io.github.achtern.AchternEngine.core.resource.fileparser.GLSLParser})
+     * @see #getShader(String, boolean, io.github.achtern.AchternEngine.core.resource.fileparser.LineBasedParser)
+     * @param name The relative path (to various ResourceLocations) of the filename
+     * @param forceLoading if set to true the file will get read again and not read from cache
+     * @return ShaderSource String
+     * @throws Exception
+     */
+    public static String getShader(String name, boolean forceLoading) throws Exception {
+        return load(name, new ShaderSourceLoader(), forceLoading);
+    }
+
+    /**
+     * Loads a shader File and prepares it for OpenGL.
+     * Calls {@link #load(String, io.github.achtern.AchternEngine.core.resource.loader.Loader, boolean)}
+     * internally and uses the {@link io.github.achtern.AchternEngine.core.resource.loader.ShaderSourceLoader}.
+     * An IOException is thrown on read errors and other Exceptions might be
+     * thrown from the ShaderSourceLoader itself.
      * @param name The relative path (to various ResourceLocations) of the filename
      * @param forceLoading if set to true the file will get read again and not read from cache
      * @param parser The optional parser to modify the shader lines.
-     * @return A readable Stream | null if not exists
-     * @throws IOException if resource not found
+     * @return ShaderSource String
+     * @throws Exception
      */
     public static String getShader(String name, boolean forceLoading, LineBasedParser parser) throws Exception {
         return load(name, new ShaderSourceLoader(parser), forceLoading);
@@ -243,8 +307,9 @@ public class ResourceLoader {
     /**
      * Reads programm file from disk and loads the stated source files
      * (uses a internal cache if the shader has been loaded previously)
-     * @param name Name of the programm declaration
-     * @return A GLSLProgramm with loaded shader sources.
+     * @see #getShaderProgram(String, boolean)
+     * @param name Name of the program declaration
+     * @return A GLSLProgram with loaded shader sources.
      * @throws IOException
      */
     public static GLSLProgram getShaderProgram(String name) throws Exception {
@@ -253,15 +318,31 @@ public class ResourceLoader {
 
     /**
      * Reads programm file from disk and loads the stated source files
-     * @param name Name of the programm declaration
+     * Calls {@link #load(String, io.github.achtern.AchternEngine.core.resource.loader.Loader, boolean)}
+     * internally and uses the {@link io.github.achtern.AchternEngine.core.resource.loader.GLSLProgramLoader}.
+     * An IOException is thrown on read errors and other Exceptions might be
+     * thrown from the GLSLProgramLoader itself.
+     * @param name Name of the program declaration
      * @param forceLoading if set to true the file will get read again and not read from cache
-     * @return A GLSLProgramm with loaded shader sources.
-     * @throws IOException
+     * @return A GLSLProgram with loaded shader sources.
+     * @throws Exception
      */
     public static GLSLProgram getShaderProgram(String name, boolean forceLoading) throws Exception {
         return load(name + SHADER_PROGRAM_EXT, new GLSLProgramLoader(), forceLoading);
     }
 
+    /**
+     * Loads a Object from a text-file using a {@link io.github.achtern.AchternEngine.core.resource.loader.Loader}.
+     * Calls {@link #readFile(String)} internally.
+     * An IOException is thrown on read errors and other Exceptions might be
+     * thrown from the Loader itself.
+     * @param name The name of the file to load
+     * @param loader The loader used to convert the file into the Object
+     * @param forceLoading if set to true the file will get read again and not read from cache
+     * @param <T> The type of Object to load
+     * @return The loaded object
+     * @throws Exception
+     */
     public static <T> T load(String name, Loader<T> loader, boolean forceLoading) throws Exception {
         String file = readFile(name, forceLoading, loader.getPreProcessor());
 
@@ -270,14 +351,40 @@ public class ResourceLoader {
         return loader.get();
     }
 
+    /**
+     * Reads a file into a String. Lines are separated by <code>\n</code>.
+     * If a file with the same name (the same file?) a cached value will be used.
+     * @param name The name of the file to load
+     * @return The read file
+     * @throws IOException
+     */
     public static String readFile(String name) throws IOException {
         return readFile(name, false);
     }
 
+    /**
+     * Reads a file into a String. Lines are separated by <code>\n</code>.
+     * If a file with the same name (the same file?) a cached value will be used.
+     * @param name The name of the file to load
+     * @param forceLoading if set to true the file will get read again and not read from cache
+     * @return The read file
+     * @throws IOException
+     */
     public static String readFile(String name, boolean forceLoading) throws IOException {
         return readFile(name, forceLoading, null);
     }
 
+    /**
+     * Reads a file into a String. Lines are separated by <code>\n</code>.
+     * If a file with the same name (the same file?) a cached value will be used.
+     * The LineBasedParser doesn't run when loaded from cache, but any modifications
+     * made by the parser are stored into the cache.
+     * @param name The name of the file to load
+     * @param forceLoading if set to true the file will get read again and not read from cache
+     * @param parser The parser gets called on every line (can be null)
+     * @return The read file
+     * @throws IOException
+     */
     public static String readFile(String name, boolean forceLoading, LineBasedParser parser) throws IOException {
         if (fileCache.has(name) && !forceLoading) {
             return fileCache.get(name);
